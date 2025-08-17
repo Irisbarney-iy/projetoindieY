1a77700d8de4ba16d079484e3faa9c8e1fb14da1
Irisbarney-iy <irisbarney.iy@gmail.com>
2025-08-16 22:59:06 -0300

    menu amburguer e modo claro e escuro (corrigir bugs no próximo commit)

--- interatividade.js
+++ interatividade.js
@@ -3,6 +3,8 @@ var botao = document.getElementById('modos')
 var a = document.querySelectorAll('#link')
 var footer = document.getElementById('footer')
 var body = document.getElementById('body')
+var caixamenu = document.getElementById('caixamenu')
+var opcoes = document.getElementsByClassName('opcoes')
 var yellow = 'rgb(249, 194, 91)'
 var corprinci = 'rgba(0, 0, 0, 0.8)'
 var corsecun = 'rgba(211, 211, 211, 0.8)'
@@ -21,6 +23,7 @@ botao.addEventListener('click', function() {
         botao.innerText = 'modo escuro'
         header.style.borderBottom = '4px solid black'
         footer.style.borderTop = '4px solid black'
+        caixamenu.style.border = '2px solid rgba(0, 0, 0, 1)'
         document.documentElement.style.setProperty ('--imagem-fundo', `url("${backgroundclaro}")`)
         document.documentElement.style.setProperty('--cor-linha', black)
         document.documentElement.style.setProperty('--cor-fundo', corsecun)
@@ -35,6 +38,7 @@ botao.addEventListener('click', function() {
         botao.innerText = 'modo claro'
         header.style.borderBottom = '4px solid rgb(249, 194, 91)'
         footer.style.borderTop = '4px solid rgb(249, 194, 91)'
+        caixamenu.style.border = '2px solid rgb(249, 194, 91)'
         document.documentElement.style.setProperty ('--imagem-fundo', `url("${background}")`)
         document.documentElement.style.setProperty('--cor-linha', 'rgb(249, 194, 91)')
         document.documentElement.style.setProperty('--cor-fundo', corprinci)
